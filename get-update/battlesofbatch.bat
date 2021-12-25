a16a608d0fac52b7e800e08dd1b352648fdc1d8b
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2021-12-26 01:24:16 +0200

    test 4
    4

--- get-update/battlesofbatch.bat
+++ get-update/battlesofbatch.bat
@@ -1,2 +1,2 @@
 SET UPDATE.VER=0.300
-SET "UPDATE.URL=http://htssoft.tk/get-download/Battles%%20Of%%20Batch%%20-%%20Manual%%20Test%%20Version.zip"
+SET "UPDATE.URL=http://htssoft.tk/get-download/Battles%%20Of%%20Batch.zip"
