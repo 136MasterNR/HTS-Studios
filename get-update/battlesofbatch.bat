bca8504c9cd2c705ae40c6fce6bf6b451d99d0ad
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2021-12-26 17:08:26 +0200

    Update 8.8

--- get-update/battlesofbatch.bat
+++ get-update/battlesofbatch.bat
@@ -1,3 +1,3 @@
-SET UPDATE.VER=0210
-SET "UPDATE.URL=http://htssoft.tk/get-download/Battles%%20Of%%20Batch%%20(v0210)%%20Tester's%%20Release%%208.zip"
+SET UPDATE.VER=0211
+SET "UPDATE.URL=http://htssoft.tk/get-download/Battles%%20Of%%20Batch%%20(v0211)%%20Tester's%%20Release%%208.zip"
 SET "CHANGELOG.URL="
