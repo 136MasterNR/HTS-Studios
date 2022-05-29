3ccb13d8a4f1ecd8e3704d223b229b4390947b4a
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2022-05-29 15:03:36 +0300

    v0250

--- get-update/battlesofbatch.bat
+++ get-update/battlesofbatch.bat
@@ -1,4 +1,4 @@
-SET UPDATE.VER=0245
+SET UPDATE.VER=0250
 SET "UPDATE.URL=https://htssoft.tk/get-download/battlesofbatch-latest.zip"
 SET "CHANGELOG.URL=https://htssoft.tk/changelog.html"
 SET RESET.DATA=TRUE
