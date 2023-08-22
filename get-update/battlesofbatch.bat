d58ce7d90f2644db5e89c175c15c0b42f36d4742
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-08-23 00:59:49 +0300

    Update BOB v0405

--- get-update/battlesofbatch.bat
+++ get-update/battlesofbatch.bat
@@ -1,4 +1,4 @@
-SET UPDATE.VER=0305
+SET UPDATE.VER=0405
 SET "UPDATE.URL=https://136masternr.github.io/HTS-Studios/get-download/battlesofbatch-latest.zip"
 SET "CHANGELOG.URL=136MasterNR/Battles-of-Batch/releases"
-SET RESET.DATA=TRUE
+SET RESET.DATA=FALSE
