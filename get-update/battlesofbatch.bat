3723c59f4a5281045b051ab3806d4095a799edf9
136 MasterGamingGR <nr136master@gmail.com>
2023-11-04 02:12:41 +0200

    Battles of Batch: release of the v0.440 update.

--- get-update/battlesofbatch.bat
+++ get-update/battlesofbatch.bat
@@ -1,4 +1,4 @@
-SET UPDATE.VER=0410
+SET UPDATE.VER=0440
 SET "UPDATE.URL=https://136masternr.github.io/HTS-Studios/get-download/battlesofbatch-latest.zip"
 SET "CHANGELOG.URL=136MasterNR/Battles-of-Batch/releases"
 SET RESET.DATA=TRUE
